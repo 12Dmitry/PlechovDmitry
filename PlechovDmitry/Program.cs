df307daaa80f789f2302feed3a4a8cfa0c9b58ab
Dmitry Plechov <dim4ik121313@gmail.com>
2022-05-20 01:46:04 +0400

    first commit

---
+++ PlechovDmitry/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PlechovDmitry
+{
+    class Program
+    {
+        static void Condition(int i)
+        {
+            if (i % 15 == 0)
+                Console.WriteLine("FizzBuzz");
+            else if (i % 3 == 0)
+                Console.WriteLine("Fizz");
+            else if (i % 5 == 0)
+                Console.WriteLine("Buzz");
+            else
+                Console.WriteLine(i);
+        }
+        static void Main(string[] args)  
+        {
+            int figure = 1;
+            while (figure<=100)
+            {
+                Condition(figure);
+                figure++;
+            }
+        }
+    }
+}
